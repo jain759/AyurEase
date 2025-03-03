52ca18d3cc5e20be36b903a1017743e9927dee14
jain759 <kri14kj@gmail.com>
2025-03-03 09:48:19 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
 document.getElementById("login-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let username = document.getElementById("login-username").value;
+    let password = document.getElementById("login-password").value;
 
     let storedUser = localStorage.getItem("registeredUser");
     let storedPass = localStorage.getItem("registeredPass");
 
     if (username === storedUser && password === storedPass) {
-        localStorage.setItem("loggedInUser", username); // Mark user as logged in
-        window.location.href = "categories.html"; // Redirect to remedies page
+        localStorage.setItem("loggedInUser", username);
+        window.location.href = "category.html"; 
     } else {
         alert("Invalid username or password!");
     }
