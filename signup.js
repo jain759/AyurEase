315815f1858492c3de15b031245044efa2ccd16d
jain759 <kri14kj@gmail.com>
2025-03-03 09:47:00 -0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,14 +1,14 @@
 document.getElementById("signup-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-    let newUsername = document.getElementById("newUsername").value;
-    let newPassword = document.getElementById("newPassword").value;
+    let newUsername = document.getElementById("signup-username").value;
+    let newPassword = document.getElementById("signup-password").value;
 
     if (newUsername && newPassword) {
         localStorage.setItem("registeredUser", newUsername);
         localStorage.setItem("registeredPass", newPassword);
         alert("Signup successful! Redirecting to login.");
-        window.location.href = "login.html"; // Redirect to login page
+        window.location.href = "login.html"; 
     } else {
         alert("Please fill in all fields.");
     }
